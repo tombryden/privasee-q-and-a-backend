3713564d6a917994ed472387416605beca2857b1
Tom <tombryden@me.com>
2024-03-12 21:41:47 +0000

    Allowed empty string to update question/answer

--- src/inputs/update-question-answer-input.ts
+++ src/inputs/update-question-answer-input.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsOptional, Length } from "class-validator";
+import { IsOptional, Length } from "class-validator";
 import { Field, ID, InputType } from "type-graphql";
 
 @InputType()
@@ -14,8 +14,8 @@ export class UpdateQuestionAnswerInput {
   question?: string;
 
   @Field({ nullable: true })
-  @Length(1, 1000, {
-    message: "Answer must be between 1 and 1000 characters",
+  @Length(0, 1000, {
+    message: "Answer must be below 1000 characters",
   })
   @IsOptional()
   answer?: string;
